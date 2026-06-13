d3792a6ad35d02ad34a38de85a47ad79e0acaf12
Amsatou Ndiaye <ndiayeamsatou26@gmail.com>
2026-06-13 22:24:27 +0000

    Remove static files storage setting to use default

--- backend/glow_beauty/settings.py
+++ backend/glow_beauty/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
